b032be4e8f4a1f409e11331ae34fccb8967289e8
Даниил Поздеев <cibertronnic@gmail.com>
2025-01-05 14:09:11 +0300

    Изменение 3

--- yatube_api/api/models.py
+++ yatube_api/api/models.py
@@ -3,23 +3,36 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+
 class Comment(models.Model):
-    post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='comments')
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(
+        'Post',
+        on_delete=models.CASCADE,
+        related_name='comments',
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    )
     text = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.text
 
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    )
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
+
 
 class Group(models.Model):
     name = models.CharField(max_length=200)
@@ -27,3 +40,4 @@ class Group(models.Model):
 
     def __str__(self):
         return self.name
+
